f1d836d5c146a59dad44d9e14c0452c81b5384ea
nastyaalmaeva <nastyaalmaeva@mail.ru>
2024-11-19 16:20:16 +0300

    Add comment in master branch

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,7 @@
 
 // This code accepts the user's name and then greets him
 int main() {
-    std::string name;
+    std::string name;  // name variable for storing the user name
     std::cout << "Enter your name: ";
     std::cin >> name;
     std::cout << "Hello, world from " << name << "!" << std::endl;
